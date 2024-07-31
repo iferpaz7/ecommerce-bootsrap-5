37d87be7b0aab4eae65bbe38d4ae3cf14367c7ee
Ivan Fernando Paz <ifer343@gmail.com>
2024-07-31 16:43:19 -0500

    Adding new design for single page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,45 @@
+//Inicia libreria Isotope para filtrar los productos
+var $grid = $(".collection-list").isotope({});
+
+$(".filter-button-group").on("click", "button", function () {
+  var filterValue = $(this).attr("data-filter");
+  resetFilterBtns();
+  $(this).addClass("active-filter-btn");
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $(".filter-button-group").find("button");
+function resetFilterBtns() {
+  filterBtns.each(function () {
+    $(this).removeClass("active-filter-btn");
+  });
+}
+
 /**
 console.log("Hola desde js");
 alert("estas seguro de empezar?");**/
 
 /**FUNCIONES */
-function loadContent(page) {
-  fetch(`/pages/${page}`)
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("main-content").innerHTML = data;
-    })
-    .catch((error) => {
-      document.getElementById("main-content").innerHTML =
-        "<h2>Error</h2><p>No se pudo cargar la página</p>";
-      console.log(error);
-    });
-}
+/* Funcion para cargar las páginas dinamicamente */
+// function loadPage() {
+//   //GRUPO DE PAGINAS DISPONIBLES
+//   let pages = ["products", "about", "contact"];
 
-loadContent("home.html");
+//   //Recorremos las paginas disponibles
+//   for (const page of pages) {
+//     fetch(`/pages/${page}.html`)
+//       .then((response) => response.text())
+//       .then((data) => {
+//         document.getElementById(page).innerHTML = data;
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
+// }
+// /*Ejecutamos la funcion al cargar el documento html */
+// loadPage();
+
+function sendMessage() {
+  alert("Gracias por su mensaje");
+}
